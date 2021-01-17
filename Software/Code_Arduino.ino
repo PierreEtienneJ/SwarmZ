22454fd72216895f64025e58b5a78ffefad51df3
Mickuits <58606639+Mickuits@users.noreply.github.com>
2021-01-17 10:35:30 +0100

    Add files via upload

---
+++ Software/Code_Arduino.ino
@@ -0,0 +1,72 @@
+ #include <Servo.h>
+#include <HCSR04.h>
+
+Servo myservo; 
+int pos = 0; 
+
+const int trigPinGauche = 52;
+const int echoPinGauche = 50;
+
+const int trigPinDroit = 46;
+const int echoPinDroit =44;
+
+UltraSonicDistanceSensor distanceSensorDroit(trigPinDroit, echoPinDroit);
+UltraSonicDistanceSensor distanceSensorGauche(trigPinGauche, echoPinGauche);
+int mesuredisDroit=0;
+int mesuredisGauche=0;
+
+
+
+const int in1 = 2;      //Pont en H
+const int in2 = 4;
+const int enable = 3;
+
+void setup() {
+  
+  myservo.attach(40);  // attaches the servo on pin 9 to the servo object
+  myservo.write(100);
+
+
+//Mcc
+    pinMode(enable, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    analogWrite(enable, 0);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  
+   Serial.begin(9600); 
+}
+
+void loop() {
+  
+ 
+mesuredisDroit=distanceSensorDroit.measureDistanceCm();
+mesuredisGauche=distanceSensorGauche.measureDistanceCm();
+
+
+if (mesuredisGauche<20 && mesuredisGauche>-1 )
+  {
+    
+      myservo.write(80);             //tourne a droite
+      analogWrite(enable, 50);       //vitesse reduite           
+    
+  }
+
+else if (mesuredisDroit<20 && mesuredisDroit>-1 )
+  {
+    
+      myservo.write(145);              //tourne a gauche 
+      analogWrite(enable, 50);          // vitesse réduite         
+    
+  }
+else 
+  {
+  myservo.write(115);                 //tuyère 0 degré
+  analogWrite(enable, 255);           //Vitesse max PWM
+  }
+
+//Serial.println(mesuredisDroit);
+//Serial.println(mesuredisGauche);
+ //Serial.println(enable);
+}
